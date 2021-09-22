70744a4f107fc471592f514e494f440fab67ba7f
Balazs <tbalazs.levente@gmail.com>
2021-09-22 19:03:18 +0200

    Feltöltes

---
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.clickcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+    private Button btnPlus;
+    private Button btnMinus;
+    private TextView clickCount;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        clickCount = findViewById(R.id.clickCount);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnMinus);
+
+        reset();
+        countPlus();
+        countMinus();
+        /*btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                clickCount.setText(Integer.toString(count));
+                colorChange();
+            }
+        });*/
+
+       /* btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count--;
+                clickCount.setText(Integer.toString(count));
+                colorChange();
+            }
+        });*/
+
+        /*clickCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count = 0;
+                clickCount.setText(Integer.toString(count));
+                colorChange();
+            }
+        });*/
+    }
+
+    private void countMinus() {
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count--;
+                clickCount.setText(Integer.toString(count));
+                colorChange();
+            }
+        });
+    }
+
+    private void countPlus() {
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                clickCount.setText(Integer.toString(count));
+                colorChange();
+            }
+        });
+    }
+
+
+    private void reset() {
+        clickCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count = 0;
+                clickCount.setText(Integer.toString(count));
+                colorChange();
+            }
+        });
+    }
+    private void colorChange() {
+        if (count == 0) {
+            clickCount.setTextColor(Color.parseColor("#FF0027FF"));
+        }
+        if (count < 0) {
+            clickCount.setTextColor(Color.parseColor("#FFFF0000"));
+        }
+        if (count > 0) {
+            clickCount.setTextColor(Color.parseColor("#FF00FF0A"));
+        }
+    }
+}
